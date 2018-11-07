eb78d7c5b9ada7552d0a97239875c47460cd9ae9
Brad Schrag <brad.schrag@gmail.com>
2018-11-07 21:14:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 # Add  code here!
 def prime?(integer)
+  return false if integer <= 0
   range = (2..integer/2).to_a
+  puts "return value is #{!(range.any? {|n| integer % n == 0})}"
   !(range.any? {|n| integer % n == 0})
 end
